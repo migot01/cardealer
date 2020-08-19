25a487979378b0610e746254f7b58117b1bedbc7
godfreyJo <godfreyoyugi@gmail.com>
2020-08-19 09:04:03 +0300

    application files

--- saroyaproject/saroyaApp/views.py
+++ saroyaproject/saroyaApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index (request): 
+    kars = Car.objects.all()
+    
+    # Cars = carListing.objects.order_by('-list_date')
+    return render(request, 'saroyaApp/index.html', {'kars':kars})
+
