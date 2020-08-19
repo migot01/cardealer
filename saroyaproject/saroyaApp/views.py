6bfa600a8d8bfe0b435c182593e945f5e5a5a24f
godfreyJo <godfreyoyugi@gmail.com>
2020-08-19 12:41:00 +0300

    making static files

--- saroyaproject/saroyaApp/views.py
+++ saroyaproject/saroyaApp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 def index (request): 
-    kars = Car.objects.all()
+    # kars = Car.objects.all()
     
-    # Cars = carListing.objects.order_by('-list_date')
-    return render(request, 'saroyaApp/index.html', {'kars':kars})
+    # # Cars = carListing.objects.order_by('-list_date') , {'kars':kars}
+    return render(request, 'saroyaApp/index-3.html')
 
